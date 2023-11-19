c645ab18a7a406bce86f3359c876d9509e5bf52e
adzoangela <angelamawufemor@gmail.com>
2023-11-18 18:53:48 -0800

    j

---
+++ nop_func.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *nop_func- null
+  *@hd: head
+  *@count: line_number
+  *Return: none
+ */
+void nop_func(stack_t **head, unsigned int count)
+{
+	(void) count;
+	(void) head;
+}
